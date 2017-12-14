1e3ca1bc3f845ccb6d293be637ed51e16eb65099
shaolizhi1234 <shaolizhi1234@126.com>
2017-12-14 20:51:28 +0800

    Initial commit

---
+++ app/src/main/java/shaolizhi/downloaddemo/MainActivity.java
@@ -0,0 +1,17 @@
+package shaolizhi.downloaddemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import okhttp3.OkHttpClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        OkHttpClient client = new OkHttpClient();
+    }
+}
