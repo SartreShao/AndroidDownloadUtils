9184aa007e047038abb0ce8ad2932dc4c12c9f75
shaolizhi1234 <shaolizhi1234@126.com>
2018-01-03 16:56:09 +0800

    添加了VideoView与运行时权限检查

--- app/src/main/java/shaolizhi/downloaddemo/MainActivity.java
+++ app/src/main/java/shaolizhi/downloaddemo/MainActivity.java
@@ -1,11 +1,20 @@
 package shaolizhi.downloaddemo;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -32,6 +41,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //检查运行时权限
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS},1);
+        }
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},2);
+        }
+
+
+        System.out.println("FUCK" + Environment.getExternalStorageDirectory().getPath());
+
+        final Button button = findViewById(R.id.button);
+
+        final VideoView videoView = findViewById(R.id.video_view);
+
+        final MediaController mediaController = new MediaController(this);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                File videoFile = new File(Environment.getExternalStorageDirectory().getPath() + File.separator + "fuck.mp4");
+                if (videoFile.exists()) {
+                    videoView.setVideoPath(videoFile.getAbsolutePath());
+                    videoView.setMediaController(mediaController);
+                    videoView.start();
+                    videoView.requestFocus();
+                } else {
+                    Toast.makeText(MainActivity.this, "下载失败", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         IOUtils.createDirectory();
         try {
             IOUtils.createFile("SHIT");
@@ -47,15 +89,6 @@ public class MainActivity extends AppCompatActivity {
 
         downloadFile.execute();
 
-//        Runnable runnable = new Runnable() {
-//            @Override
-//            public void run() {
-//
-//            }
-//        };
-//
-//        Thread thread = new Thread(runnable);
-//        thread.start();
     }
 
     private static boolean writeResponseBodyToDisk(ResponseBody body) {
